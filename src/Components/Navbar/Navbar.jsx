a65b357df2a904f63539d48038f8ba68d45c3042
dixon310 <dixonchiu22@hotmail.com>
2025-02-10 23:29:45 +1100

    feat: enhance Navbar with smooth scrolling and update component IDs for better navigation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
 import './Navbar.css';
 import logo from '../../assets/logo.svg';
+import underline from '../../assets/nav_underline.svg';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState('home');
+
   return (
     <div className='navbar'>
       <img src={logo} alt="Logo" />
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About me</li>
-        <li>Services</li>
-        <li>My Work</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li>
+          <Link className='anchor-link' to='home' smooth={true} duration={1000} onClick={() => setMenu("home")}>
+            <p>Home</p>x
+          </Link>
+          {menu === "home" && <img src={underline} alt='underline' />}
+        </li>
+        <li>
+          <Link className='anchor-link' to='about' smooth={true} duration={1000} onClick={() => setMenu("about")}>
+            <p>About me</p>
+          </Link>
+          {menu === "about" && <img src={underline} alt='underline' />}
+        </li>
+        <li>
+          <Link className='anchor-link' to='services' smooth={true} duration={1000} onClick={() => setMenu("services")}>
+            <p>Services</p>
+          </Link>
+          {menu === "services" && <img src={underline} alt='underline' />}
+        </li>
+        <li>
+          <Link className='anchor-link' to='work' smooth={true} duration={1000} onClick={() => setMenu("work")}>
+            <p>Portfolio</p>
+          </Link>
+          {menu === "work" && <img src={underline} alt='underline' />}
+        </li>
+        <li>
+          <Link className='anchor-link' to='contact' smooth={true} duration={1000} onClick={() => setMenu("contact")}>
+            <p>Contact</p>
+          </Link>
+          {menu === "contact" && <img src={underline} alt='underline' />}
+        </li>
       </ul>
+      <Link className='anchor-link' to='contact' smooth={true} duration={1000} onClick={() => setMenu("contact")}>
       <div className="nav-connect">Connect With Me</div>
+      </Link>
     </div>
   );
 };
